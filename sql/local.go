6074ce29013cb1655bf9cf4cc7152f1dda2ca183
pstrzalk <p.strzalkowski@visuality.pl>
2018-12-27 09:47:02 +0100

    Add sql

---
+++ sql/local.go
@@ -0,0 +1,119 @@
+// export GOOGLE_APPLICATION_CREDENTIALS=credentials.json
+
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Command caption sends audio data to the Google Speech API
+// and prints its transcript.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	speech "cloud.google.com/go/speech/apiv1"
+	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+)
+
+const usage = `Usage: caption <audiofile>`
+
+func main() {
+	flag.Parse()
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(2)
+	}
+
+	var runFunc func(io.Writer, string) error
+	path := os.Args[1]
+	if strings.Contains(path, "://") {
+		runFunc = recognizeGCS
+	} else {
+		runFunc = recognize
+	}
+
+	// Perform the request.
+	if err := runFunc(os.Stdout, os.Args[1]); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// [START speech_transcribe_sync_gcs]
+
+func recognizeGCS(w io.Writer, gcsURI string) error {
+	ctx := context.Background()
+	client, err := speech.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Send the request with the URI (gs://...)
+	// and sample rate information to be transcripted.
+	resp, err := client.Recognize(ctx, &speechpb.RecognizeRequest{
+		Config: &speechpb.RecognitionConfig{
+			// Encoding:        speechpb.RecognitionConfig_FLAC,
+			// SampleRateHertz: 16000,
+			LanguageCode:    "pl-PL",
+		},
+		Audio: &speechpb.RecognitionAudio{
+			AudioSource: &speechpb.RecognitionAudio_Uri{Uri: gcsURI},
+		},
+	})
+
+	// Print the results.
+	for _, result := range resp.Results {
+		for _, alt := range result.Alternatives {
+			fmt.Fprintf(w, "\"%v\" (confidence=%3f)\n", alt.Transcript, alt.Confidence)
+		}
+	}
+	return nil
+}
+
+// [END speech_transcribe_sync_gcs]
+
+// [START speech_transcribe_sync]
+
+func recognize(w io.Writer, file string) error {
+	ctx := context.Background()
+	client, err := speech.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	// Send the contents of the audio file with the encoding and
+	// and sample rate information to be transcripted.
+	resp, err := client.Recognize(ctx, &speechpb.RecognizeRequest{
+		Config: &speechpb.RecognitionConfig{
+			Encoding:        speechpb.RecognitionConfig_LINEAR16,
+			LanguageCode:    "pl-PL",
+		},
+		Audio: &speechpb.RecognitionAudio{
+			AudioSource: &speechpb.RecognitionAudio_Content{Content: data},
+		},
+	})
+  if err != nil {
+		return err
+	}
+
+	// Print the results.
+	for _, result := range resp.Results {
+		for _, alt := range result.Alternatives {
+			fmt.Fprintf(w, "\"%v\" (confidence=%3f)\n", alt.Transcript, alt.Confidence)
+		}
+	}
+	return nil
+}
+
+// [END speech_transcribe_sync]
